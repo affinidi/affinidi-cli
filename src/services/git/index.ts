0551fe6c1cfdf16d666808dc49dc3c54874e4b56
denis.d <denis.d@affinidi.com>
2022-11-11 12:58:24 +0100

    fix: add support for spaces for generated app path

--- src/services/git/index.ts
+++ src/services/git/index.ts
@@ -3,7 +3,7 @@ import { exec } from 'child_process'
 export class GitService {
   public static clone = async (repository: string, destination: string): Promise<void> => {
     return new Promise((resolve, reject) => {
-      exec(`git clone ${repository} ${destination}`, (error) => {
+      exec(`git clone ${repository} "${destination}"`, (error) => {
         if (error) {
           reject(error)
         }
