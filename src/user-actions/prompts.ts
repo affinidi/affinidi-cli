cc36b77b3415ed03d9a362adb1eba3144e6cab2e
YoussefWasfy <youssef.w@affinidi.com>
2022-11-23 11:57:20 +0100

    fix: fix tests

--- src/user-actions/prompts.ts
+++ src/user-actions/prompts.ts
@@ -5,19 +5,17 @@ import { conditionsAndPolicyMessage } from '../render/texts'
 export const AnswerNo = 'n'
 export const AnswerYes = 'y'
 
-type YesOrNo = 'n' | 'y'
-
 export const enterEmailPrompt = async (
   text: string = 'Enter your Affinidi email address',
 ): Promise<string> => {
   return CliUx.ux.prompt(text, { required: true })
 }
 
-export const enterOTPPrompt = async (): Promise<YesOrNo> => {
+export const enterOTPPrompt = async (): Promise<string> => {
   return CliUx.ux.prompt('Enter the confirmation code we emailed to you', { type: 'mask' })
 }
 
-export const confirmSignOut = async (): Promise<YesOrNo> => {
+export const confirmSignOut = async (): Promise<string> => {
   return CliUx.ux.prompt('Please confirm that you want to sign-out from Affinidi [Y/n]', {
     default: 'y',
   })
