7f6a8d40606d5e1e6398b772294526a024b6e018
YoussefAWasfy <112941926+YoussefAWasfy@users.noreply.github.com>
2022-11-23 16:54:45 +0100

    feat:  add auto-completion to cli [FTL-7958] (#5)
    finally understood how it works .... 
    sorry for the delay

---
+++ scripts/autocomplete.cjs
@@ -0,0 +1,29 @@
+const { exec } = require('child_process')
+
+exec('echo $SHELL', (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    return
+  }
+  
+  if(stdout.includes('zsh')){
+    execEnvVarCommand('zsh')
+    return
+  }
+  if(stdout.includes('bash')){
+    execEnvVarCommand('bash')
+    return
+  }
+  console.log('Auto-completion supports only zsh or bash shells.')
+})
+
+const execEnvVarCommand = (shell) => {
+  exec(`printf "eval $(affinidi autocomplete:script ${shell})" >> ~/.${shell}rc; source ~/.${shell}rc`,(err, stdout, stderr) =>{
+  if (err){
+    return
+  }
+  if (stderr){
+    console.log("Failed to activate auto-completion")
+  }
+  return
+} )}
--- src/user-actions/prompts.ts
+++ src/user-actions/prompts.ts
@@ -5,8 +5,6 @@ import { conditionsAndPolicyMessage } from '../render/texts'
 export const AnswerNo = 'n'
 export const AnswerYes = 'y'
 
-type YesOrNo = 'n' | 'y'
-
 export const enterEmailPrompt = async (
   text: string = 'Enter your Affinidi email address',
 ): Promise<string> => {
@@ -17,13 +15,13 @@ export const enterOTPPrompt = async (): Promise<string> => {
   return CliUx.ux.prompt('Enter the confirmation code we emailed to you', { type: 'mask' })
 }
 
-export const confirmSignOut = async (): Promise<YesOrNo> => {
+export const confirmSignOut = async (): Promise<string> => {
   return CliUx.ux.prompt('Please confirm that you want to sign-out from Affinidi [Y/n]', {
     default: 'y',
   })
 }
 
-export const acceptConditionsAndPolicy = async (): Promise<YesOrNo> => {
+export const acceptConditionsAndPolicy = async (): Promise<string> => {
   return CliUx.ux.prompt(conditionsAndPolicyMessage, {
     default: 'n',
   })
