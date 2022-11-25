e6550eada2b14848b8541937a0c1b169b8e88361
aeffinidi <86773100+aeffinidi@users.noreply.github.com>
2022-11-25 15:19:23 +0100

    Revert "fix:  add prompt for auto completion [FTL-7958] (#14)"
    This reverts commit 88acebf07c3513f9f952a96b9adc343d6bdce87f.

--- scripts/autocomplete.cjs
+++ scripts/autocomplete.cjs
@@ -1,15 +1,11 @@
 const { exec } = require('child_process')
-const {CliUx} = require('@oclif/core')
 
-exec('echo $SHELL', async (err, stdout, stderr)  => {
+exec('echo $SHELL', (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
     return
   }
-    const answers = await CliUx.ux.prompt(`Please confirm that you want to activate autocompletion [Y/n].(it will add several lines to your ./${stdout.split('/').at(-1)}rc file`, {
-      default: 'n',
-    })
-  if (answers.toLowerCase() === 'y'){
+
   if(stdout.includes('zsh')){
     execEnvVarCommand('zsh')
     return
@@ -18,8 +14,7 @@ exec('echo $SHELL', async (err, stdout, stderr)  => {
     execEnvVarCommand('bash')
     return
   }
-  console.log('Auto-completion supports only zsh or bash shells.') }
-  console.log('Auto-completion not activated. To activate in future run npm install')
+  console.log('Auto-completion supports only zsh or bash shells.')
 })
 
 const execEnvVarCommand = (shell) => {
