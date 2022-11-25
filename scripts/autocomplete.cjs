33dbc4fee05b691b1336eb7ef141f86ac0f31490
YoussefWasfy <youssef.w@affinidi.com>
2022-11-25 14:50:13 +0100

    fix: add prompt for activating autocompletion

--- scripts/autocomplete.cjs
+++ scripts/autocomplete.cjs
@@ -1,11 +1,15 @@
 const { exec } = require('child_process')
+const {CliUx} = require('@oclif/core')
 
-exec('echo $SHELL', (err, stdout, stderr) => {
+exec('echo $SHELL', async (err, stdout, stderr)  => {
   if (err) {
     // node couldn't execute the command
     return
   }
-
+    const answers = await CliUx.ux.prompt(`Please confirm that you want to activate autocompletion [Y/n].(it will add several lines to your ./${stdout.split('/').at(-1)}rc file`, {
+      default: 'n',
+    })
+  if (answers.toLowerCase() === 'y'){
   if(stdout.includes('zsh')){
     execEnvVarCommand('zsh')
     return
@@ -14,7 +18,8 @@ exec('echo $SHELL', (err, stdout, stderr) => {
     execEnvVarCommand('bash')
     return
   }
-  console.log('Auto-completion supports only zsh or bash shells.')
+  console.log('Auto-completion supports only zsh or bash shells.') }
+  console.log('Auto-completion not activated. To activate in future run npm install')
 })
 
 const execEnvVarCommand = (shell) => {
