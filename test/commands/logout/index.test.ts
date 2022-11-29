9dcf94c26ec8bf5b2a4eb41d0ba4d66cbf66ed87
Atsuhiro Endo <atsuhiro.e@affinidi.com>
2022-11-29 16:05:50 +0100

    fix: test the config file after signing-out

--- test/commands/logout/index.test.ts
+++ test/commands/logout/index.test.ts
@@ -3,13 +3,22 @@ import { StatusCodes } from 'http-status-codes'
 import { expect } from 'chai'
 
 import { ANALYTICS_URL } from '../../../src/services/analytics'
-import { createSession, USER_MANAGEMENT_URL } from '../../../src/services/user-management'
+import {
+  createConfig,
+  createSession,
+  USER_MANAGEMENT_URL,
+} from '../../../src/services/user-management'
 import * as prompts from '../../../src/user-actions'
 import { vaultService, VAULT_KEYS } from '../../../src/services'
+import { configService, getMajorVersion } from '../../../src/services/config'
+
+const testUserId = '38efcc70-bbe1-457a-a6c7-b29ad9913648'
 
 describe('logout command', () => {
   before(() => {
-    createSession('email', 'userId', 'sessionToken')
+    createSession('email', testUserId, 'sessionToken')
+    createConfig({ userId: testUserId })
+    // configService.
   })
   test
     .nock(`${USER_MANAGEMENT_URL}`, (api) => api.post('/auth/logout').reply(StatusCodes.CREATED))
@@ -26,5 +35,13 @@ describe('logout command', () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
       expect(vaultService.get(k)).to.be.null
     })
+    // TODO check that the config is still there
+    const config = configService.show()
+    expect(config.currentUserId).to.equal(testUserId)
+    expect(config.version).to.equal(getMajorVersion())
+    expect(config.version).to.equal(3)
+    expect(config.configs).to.haveOwnProperty(testUserId)
+    expect(config.configs[testUserId].activeProjectId).to.equal('')
+    expect(config.configs[testUserId].outputFormat).to.equal('plaintext')
   })
 })
