98fc5f0af63d1f6fa71ba816ed3cc30c6b336a4e
Atsuhiro Endo <atsuhiro.e@affinidi.com>
2022-11-29 09:01:00 +0100

    fix: returning hard coded version number

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,1 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import * as fs from 'fs'
-import rootPath from 'app-root-path'
-
-const loadJSON = (path: string): any => {
-  try {
-    const d = fs.readFileSync(path, 'utf8')
-    return JSON.parse(d)
-  } catch (err) {
-    console.error('impossible to load json file', err)
-    return undefined
-  }
-}
-
-const packageJsonPath = `${rootPath}/package.json`
-const pjson = loadJSON(packageJsonPath)
-export const version = pjson && pjson.version ? pjson.version : '0.0.0'
+export const version = 1
