fd5223df023492f0b4cc55578a8fdfe6278705e3
Denis Davydov <88322488+dendavidov-affinidi@users.noreply.github.com>
2022-11-29 11:57:18 +0100

    chore: ad dependabot

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     // more rules: https://commitlint.js.org/#/reference-rules
     rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'chore', 'docs']],
+        'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'chore', 'docs', "test", "build"]],
         'subject-empty': [2, 'never'],
     },
 };
