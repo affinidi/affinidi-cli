9e7ef2dfd1a97c9a24aa1f749696d4180ae236a7
YoussefWasfy <youssef.w@affinidi.com>
2022-12-15 17:31:52 +0100

    feat: add main  menu

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -8,6 +8,7 @@ import SignUp from './sign-up'
 import { getSession } from '../services/user-management'
 import { iAmService } from '../services'
 import CreateProject from './create/project'
+import Logout from './logout'
 
 export default class Start extends Command {
   static description = 'describe the command here'
@@ -17,15 +18,18 @@ export default class Start extends Command {
   static isWizard = true
 
   public async run(): Promise<void> {
+    let breadCrumbs: string[]
     if (!isAuthenticated()) {
-      CliUx.ux.info(wizardStatusMessage(wizardStatus({ breadcrumbs: [] })))
+      CliUx.ux.info(wizardStatusMessage(wizardStatus({ breadcrumbs: breadCrumbs })))
       const nextStep = await selectNextStep(['login', 'sign-up'])
       switch (nextStep) {
         case 'login':
           Login.run(['-w'])
+          breadCrumbs.push(nextStep)
           break
         default:
           SignUp.run(['-w'])
+          breadCrumbs.push(nextStep)
           break
       }
     }
@@ -33,7 +37,33 @@ export default class Start extends Command {
     const projects = await iAmService.listProjects(token, 0, 10)
     if (projects.length === 0) {
       CreateProject.run([])
-    } else {
+      breadCrumbs.push('create a project')
+    }
+    let nextStep = await selectNextStep([
+      'manage projects',
+      'manage schemas',
+      'generate an application',
+      'issue a vc',
+      'verify a vc',
+      'logout',
+    ])
+    switch (nextStep) {
+      case 'manage projects':
+        break
+      case 'manage schemas':
+        break
+      case 'generate an application':
+        break
+      case 'issue a vc':
+        break
+      case 'verify a vc':
+        break
+      case 'logout':
+        Logout.run([])
+        breadCrumbs.push(nextStep)
+        break
+      default:
+        break
     }
   }
 }
