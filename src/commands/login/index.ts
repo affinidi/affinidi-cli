19d495437de2245e3452377a6d0d4bd08f25a178
YoussefWasfy <youssef.w@affinidi.com>
2022-12-22 09:59:59 +0100

    fix: refactor and add optional chaining

--- src/commands/login/index.ts
+++ src/commands/login/index.ts
@@ -99,11 +99,9 @@ export default class Login extends Command {
     const sessionWithoutPrefix = sessionToken.replace('console_authtoken=', '')
     vaultService.clear()
     createSession(email, userId, sessionWithoutPrefix)
-    if (
-      confVersionError ||
-      !configService.show().configs[userId]?.analyticsOptIn ||
-      !configService.show().configs[userId]?.activeProjectId
-    ) {
+    const userConfig = configService.show().configs[userId]
+
+    if (confVersionError || !userConfig?.analyticsOptIn || !userConfig?.activeProjectId) {
       const wantsToOptIn = await analyticsConsentPrompt()
       createOrUpdateConfig({ userId, analyticsOptIn: wantsToOptIn })
     }
