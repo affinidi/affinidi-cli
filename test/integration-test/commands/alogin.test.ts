7cdd67c6c75729c084c81105906d84a04fd9d1bb
YoussefWasfy <youssef.w@affinidi.com>
2023-01-20 14:37:41 +0100

    fix: increase delay for login input

--- test/integration-test/commands/alogin.test.ts
+++ test/integration-test/commands/alogin.test.ts
@@ -14,7 +14,7 @@ const getCode = async (): Promise<string> => {
 describe('User login', () => {
   test
     .stdout()
-    .stdin('\n', 30000)
+    .stdin('\n', 35000)
     .stub(prompts, 'analyticsConsentPrompt', () => async () => prompts.AnswerNo)
     .stub(prompts, 'enterOTPPrompt', () => async () => getCode())
     .stub(prompts, 'confirmSignOut', () => async () => prompts.AnswerYes)
