8cefb5cba05db371ebaac863e5b8d465feb58eab
Pulkit Batra <pulkit.b@affinidi.com>
2024-02-06 14:53:00 +0530

    feat: add update-project command, remove edit-project commands

--- src/commands/project/update-project.ts
+++ src/commands/project/update-project.ts
@@ -7,15 +7,19 @@ import { bffService } from '../../services/affinidi/bff-service'
 import { ProjectDto } from '../../services/affinidi/iam/iam.api'
 
 export class UpdateProject extends BaseCommand<typeof UpdateProject> {
-  static summary = 'Updates project description'
+  static summary = 'Updates project details'
   static examples = [
-    '<%= config.bin %> <%= command.id %> -d MyUpdatedProjectDescription',
-    '<%= config.bin %> <%= command.id %> --description "My Updated Project Description"',
+    '<%= config.bin %> <%= command.id %> -n MyUpdatedProjectName -d MyUpdatedProjectDescription',
+    '<%= config.bin %> <%= command.id %> --name="My project name" --description="My project description',
   ]
   static flags = {
     id: Flags.string({
       char: 'i',
       summary: 'Project Id',
+    }),
+    name: Flags.string({
+      char: 'n',
+      summary: 'Name of the project',
     }),
     description: Flags.string({
       char: 'd',
@@ -25,19 +29,20 @@ export class UpdateProject extends BaseCommand<typeof UpdateProject> {
 
   public async run(): Promise<ProjectDto> {
     const { flags } = await this.parse(UpdateProject)
-    const promptFlags = await promptRequiredParameters(['id', 'description'], flags)
+    const promptFlags = await promptRequiredParameters(['id'], flags)
 
     const schema = z.object({
       id: z.string().max(INPUT_LIMIT),
-      description: z.string().max(INPUT_LIMIT),
+      name: z.string().max(INPUT_LIMIT).optional(),
+      description: z.string().max(INPUT_LIMIT).optional(),
     })
     const validatedFlags = schema.parse(promptFlags)
 
-    ux.action.start('Updating the project description')
+    ux.action.start('Updating the project')
 
     const updateProjectOutput = await bffService.updateProject({
       id: validatedFlags.id,
-      description: validatedFlags.description,
+      name: validatedFlags.name,
     })
 
     ux.action.stop('Updated successfully!')
