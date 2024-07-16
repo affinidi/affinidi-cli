6e39985f0cd46750e8a15cecdbe4c4e057926d61
Carlos Rincon <86779651+carlos-affinidi@users.noreply.github.com>
2024-07-16 11:41:53 +0200

    feat: generate app with token (#395)
    Co-authored-by: Carlos Rincon <“carlos.r@affinidi.com”>

---
+++ src/helpers/token.ts
@@ -0,0 +1,92 @@
+import { KeyExportOptions } from 'crypto'
+import { generateKeyPairSync } from 'node:crypto'
+import { SupportedAlgorithms } from '../common'
+import { policiesDataSchema } from '../common/validators'
+import { JsonWebKeySetDto } from '../services/affinidi/iam/iam.api'
+import { iamService } from '../services/affinidi/iam/index'
+
+export function generateKeyPair(keyId: string, algorithm: string, passphrase?: string) {
+  const { publicKey, privateKey } = generateKeyPairSync('rsa', { modulusLength: 4096 })
+
+  const publicKeyPem = publicKey.export({ format: 'pem', type: 'spki' })
+  const exportOptions: KeyExportOptions<'pem'> = {
+    format: 'pem',
+    type: 'pkcs8',
+  }
+  if (passphrase) {
+    exportOptions.cipher = 'aes-256-cbc'
+    exportOptions.passphrase = passphrase
+  }
+  const privateKeyPem = privateKey.export(exportOptions)
+  const publicKeyJwk = publicKey.export({ format: 'jwk' })
+
+  const jwks = {
+    keys: [
+      {
+        use: 'sig',
+        kid: keyId,
+        alg: algorithm,
+        ...publicKeyJwk,
+      },
+    ],
+  }
+
+  return { publicKey: publicKeyPem, privateKey: privateKeyPem, jwks }
+}
+
+export async function createToken(
+  name: string,
+  algorithm: SupportedAlgorithms,
+  jwks: JsonWebKeySetDto,
+  keyId?: string,
+) {
+  let token = await iamService.createToken({
+    name,
+    authenticationMethod: {
+      type: 'PRIVATE_KEY',
+      signingAlgorithm: algorithm,
+      publicKeyInfo: {
+        jwks,
+      },
+    },
+  })
+  // If keyId was not provided it means that a randomly generated uuid was assigned and we should change it to the tokenId
+  if (!keyId) {
+    jwks.keys[0].kid = token.id
+    token = await iamService.updateToken(token.id, {
+      name,
+      authenticationMethod: {
+        type: 'PRIVATE_KEY',
+        signingAlgorithm: algorithm,
+        publicKeyInfo: {
+          jwks,
+        },
+      },
+    })
+  }
+  return token
+}
+
+export async function addPrincipal(tokenId: string) {
+  await iamService.addPrincipalToProject({
+    principalId: tokenId,
+    principalType: 'token',
+  })
+}
+
+export async function updatePolicies(tokenId: string, activeProjectId: string, actions: string[], resources: string[]) {
+  const policiesData = {
+    version: '2022-12-15',
+    statement: [
+      {
+        principal: [`ari:iam::${activeProjectId}:token/${tokenId}`],
+        action: actions,
+        resource: resources,
+        effect: 'Allow',
+      },
+    ],
+  }
+  const validatedPolicies = policiesDataSchema.parse(policiesData)
+  const result = await iamService.updatePolicies(tokenId, 'token', validatedPolicies)
+  return result
+}
