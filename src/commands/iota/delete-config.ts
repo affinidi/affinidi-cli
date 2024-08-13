b519ddcf7cb16d25b616c1f273fb7a42a22386e8
Roman Brazhnyk <roman.b@affinidi.com>
2024-08-13 15:08:24 +0800

    feat: add commands to manage pex queries, iota configs

---
+++ src/commands/iota/delete-config.ts
@@ -0,0 +1,36 @@
+import { ux, Flags } from '@oclif/core'
+import z from 'zod'
+import { BaseCommand } from '../../common/base-command.js'
+import { promptRequiredParameters } from '../../common/prompts.js'
+import { INPUT_LIMIT } from '../../common/validators.js'
+import { iotaService } from '../../services/affinidi/iota/service.js'
+
+export class DeleteIotaConfig extends BaseCommand<typeof DeleteIotaConfig> {
+  static summary = 'Deletes Iota configuration from your active project'
+  static examples = [
+    '<%= config.bin %> <%= command.id %> -i <value>',
+    '<%= config.bin %> <%= command.id %> --id <value>',
+  ]
+  static flags = {
+    id: Flags.string({
+      char: 'i',
+      summary: 'ID of the Iota configuration',
+    }),
+  }
+
+  public async run(): Promise<{ id: string }> {
+    const { flags } = await this.parse(DeleteIotaConfig)
+    const promptFlags = await promptRequiredParameters(['id'], flags)
+    const schema = z.object({
+      id: z.string().max(INPUT_LIMIT),
+    })
+    const validatedFlags = schema.parse(promptFlags)
+
+    ux.action.start('Deleting Iota configurations')
+    await iotaService.deleteIotaConfigById(validatedFlags.id)
+    ux.action.stop('Deleted successfully!')
+
+    if (!this.jsonEnabled()) this.logJson({ id: validatedFlags.id })
+    return { id: validatedFlags.id }
+  }
+}
---
+++ src/commands/iota/delete-pex-query.ts
@@ -0,0 +1,36 @@
+import { ux, Flags } from '@oclif/core'
+import z from 'zod'
+import { BaseCommand } from '../../common/base-command.js'
+import { promptRequiredParameters } from '../../common/prompts.js'
+import { INPUT_LIMIT } from '../../common/validators.js'
+import { iotaService } from '../../services/affinidi/iota/service.js'
+
+export class DeletePexQuery extends BaseCommand<typeof DeletePexQuery> {
+  static summary = 'Deletes the PEX query from your Iota configuration'
+  static examples = ['<%= config.bin %> <%= command.id %> --configuration-id <uuid> --query-id <uuid>']
+  static flags = {
+    'configuration-id': Flags.string({
+      summary: 'ID of the Iota configuration',
+    }),
+    'query-id': Flags.string({
+      summary: 'PEX query ID',
+    }),
+  }
+
+  public async run(): Promise<{ queryId: string }> {
+    const { flags } = await this.parse(DeletePexQuery)
+    const promptFlags = await promptRequiredParameters(['configuration-id', 'query-id'], flags)
+    const schema = z.object({
+      'configuration-id': z.string().max(INPUT_LIMIT).uuid(),
+      'query-id': z.string().max(INPUT_LIMIT).uuid(),
+    })
+    const validatedFlags = schema.parse(promptFlags)
+
+    ux.action.start('Deleting PEX queries')
+    await iotaService.deletePexQueryById(validatedFlags['configuration-id'], validatedFlags['query-id'])
+    ux.action.stop('Deleted successfully!')
+
+    if (!this.jsonEnabled()) this.logJson({ queryId: validatedFlags['query-id'] })
+    return { queryId: validatedFlags['query-id'] }
+  }
+}
